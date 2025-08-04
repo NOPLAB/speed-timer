f8a8110ae4ca392b345e9c601657240ceed64797
nop <noplab90@gmail.com>
2025-08-04 16:33:03 +0900

    BLE

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ag_lcd::LcdDisplay;
 use esp_idf_svc::hal::{delay::Delay, i2c::{I2cConfig, I2cDriver}, prelude::Peripherals,  timer::*};
 use port_expander::Pcf8574;
 use esp_idf_svc::hal::prelude::*;
+use esp32_nimble::{uuid128, BLEAdvertisementData, BLEDevice, NimbleProperties};
 
 static mut TIMER_COUNTER: u64 = 0;
 
@@ -12,6 +13,72 @@ fn main() -> anyhow::Result<()> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+let ble_device = BLEDevice::take();
+  let ble_advertising = ble_device.get_advertising();
+
+  let server = ble_device.get_server();
+  server.on_connect(|server, desc| {
+    ::log::info!("Client connected: {:?}", desc);
+
+    server
+      .update_conn_params(desc.conn_handle(), 24, 48, 0, 60)
+      .unwrap();
+
+    if server.connected_count() < (esp_idf_svc::sys::CONFIG_BT_NIMBLE_MAX_CONNECTIONS as _) {
+      ::log::info!("Multi-connect support: start advertising");
+      ble_advertising.lock().start().unwrap();
+    }
+  });
+
+  server.on_disconnect(|_desc, reason| {
+    ::log::info!("Client disconnected ({:?})", reason);
+  });
+
+  let service = server.create_service(uuid128!("fafafafa-fafa-fafa-fafa-fafafafafafa"));
+
+  // A static characteristic.
+  let static_characteristic = service.lock().create_characteristic(
+    uuid128!("d4e0e0d0-1a2b-11e9-ab14-d663bd873d93"),
+    NimbleProperties::READ,
+  );
+  static_characteristic
+    .lock()
+    .set_value("Hello, world!".as_bytes());
+
+  // A characteristic that notifies every second.
+  let notifying_characteristic = service.lock().create_characteristic(
+    uuid128!("a3c87500-8ed3-4bdf-8a39-a01bebede295"),
+    NimbleProperties::READ | NimbleProperties::NOTIFY,
+  );
+  notifying_characteristic.lock().set_value(b"Initial value.");
+
+  // A writable characteristic.
+  let writable_characteristic = service.lock().create_characteristic(
+    uuid128!("3c9a3f00-8ed3-4bdf-8a39-a01bebede295"),
+    NimbleProperties::READ | NimbleProperties::WRITE,
+  );
+  writable_characteristic
+    .lock()
+    .on_read(move |_, _| {
+      ::log::info!("Read from writable characteristic.");
+    })
+    .on_write(|args| {
+      ::log::info!(
+        "Wrote to writable characteristic: {:?} -> {:?}",
+        args.current_data(),
+        args.recv_data()
+      );
+    });
+
+  ble_advertising.lock().set_data(
+    BLEAdvertisementData::new()
+      .name("ESP32-GATT-Server")
+      .add_service_uuid(uuid128!("fafafafa-fafa-fafa-fafa-fafafafafafa")),
+  )?;
+  ble_advertising.lock().start()?;
+
+  server.ble_gatts_show_local();
 
     let p = Peripherals::take()?;
 
@@ -59,5 +126,11 @@ fn main() -> anyhow::Result<()> {
         let text = format!("Time: {}.{}", seconds, millis);
 
         lcd.print(&text);
+
+ notifying_characteristic
+      .lock()
+
+      .set_value(&cnt.to_le_bytes())
+      .notify();
     }
 }
